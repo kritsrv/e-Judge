51a19185a538b765e594cafa294c0064e237b32c
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:48:15 +0700

    Upload New File

---
+++ e-Judge/Week4/factorial.py
@@ -0,0 +1,9 @@
+""" e-Judge """
+def main():
+    """ Factorial """
+    number = int(input())
+    factorial = 1
+    for i in range(1, number+1):
+        factorial *= i
+    print(factorial)
+main()
