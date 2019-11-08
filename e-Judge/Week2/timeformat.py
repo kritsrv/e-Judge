eff88ffcfa447f263cb4d78cd4b2fd5bc7c756f0
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:46:12 +0700

    Upload New File

---
+++ e-Judge/Week2/timeformat.py
@@ -0,0 +1,7 @@
+""" e-Judge """
+def main():
+    """ Time format """
+    number_hour = int(input())
+    number_minutes = int(input())
+    print("%02d:%02d" %(number_hour, number_minutes))
+main()
