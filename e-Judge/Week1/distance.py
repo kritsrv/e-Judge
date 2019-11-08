57b8486803087e30b4f772f7aed3292853b13111
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:43:01 +0700

    Upload New File

---
+++ e-Judge/Week1/distance.py
@@ -0,0 +1,8 @@
+""" e-Judge """
+def main():
+    """ Distance """
+    distance_1 = float(input())
+    distance_2 = float(input())
+    submission = abs(distance_1 - distance_2)
+    print(submission)
+main()
