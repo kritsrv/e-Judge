984dc8f6df0e12027c41ab0d40599f2e52c174a2
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:39:38 +0700

    Upload New File

---
+++ e-Judge/cylinder.py
@@ -0,0 +1,9 @@
+""" e-Judge """
+import math as m
+def main():
+    """ Cylinder """
+    radius = float(input())
+    height = float(input())
+    volume = m.pi * radius**2 * height
+    print(volume)
+main()
