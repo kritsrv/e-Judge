c0e1c07a8c4f1dbc924f28aea79f42c5162bebf2
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:42:31 +0700

    Delete pluswithoutaddition.py

--- e-Judge/pluswithoutaddition.py
+++
@@ -1,8 +0,0 @@
-""" e-Judge """
-def main():
-    """ Plus without addition """
-    number_a = float(input())
-    number_b = float(input())
-    plus = number_a - (-number_b)
-    print(plus)
-main()
