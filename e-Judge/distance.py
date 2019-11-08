cb2880f60fbb8e4f409974723a39675edbe78f0a
นายสรวิศ ยินดีอนันต์ <62070277@kmitl.ac.th>
2019-11-08 12:42:11 +0700

    Delete distance.py

--- e-Judge/distance.py
+++
@@ -1,8 +0,0 @@
-""" e-Judge """
-def main():
-    """ Distance """
-    distance_1 = float(input())
-    distance_2 = float(input())
-    submission = abs(distance_1 - distance_2)
-    print(submission)
-main()
